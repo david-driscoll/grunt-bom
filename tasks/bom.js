f38bec44cf3dea4fb1f3878ea309d1c30d315816
David Driscoll <david.driscoll@gmail.com>
2012-12-30 14:28:41 -0700

    Updated grunt-bom to work with grunt 0.4.x

--- tasks/bom.js
+++ tasks/bom.js
@@ -1,46 +1,31 @@
-/*
- * grunt
- * https://github.com/cowboy/grunt
- *
- * Copyright (c) 2012 "Cowboy" Ben Alman
- * Licensed under the MIT license.
- * http://benalman.com/about/license/
- */
+module.exports = function (grunt)
+{
 
-module.exports = function(grunt) {
+    // ==========================================================================
+    // TASKS
+    // ==========================================================================
 
-  // ==========================================================================
-  // TASKS
-  // ==========================================================================
+    grunt.registerMultiTask('bom', 'byte order mark remove files.', function ()
+    {
+        var files = this.file.src;
+        files.map(function (filepath)
+        {
+            if (!grunt.file.exists(filepath))
+            {
+                grunt.log.error('Source file "' + filepath + '" not found.');
+                return '';
+            }
 
-  grunt.registerMultiTask('bom', 'Concatenate files.', function() {
-    var files = grunt.file.expandFiles(this.file.src);
-    // Concat specified files.
-    grunt.helper('bom', files);
+            var content = grunt.file.read(filepath);
+            if (/^\uFEFF/.test(content))
+            {
+                content = content.replace(/^\uFEFF/, '');
+                grunt.file.write(filepath, content);
+                grunt.log.writeln('File "' + filepath + '" rewritten.');
+            }
+        });
 
-    // Fail task if errors were logged.
-    if (this.errorCount) { return false; }
-  });
-
-  // ==========================================================================
-  // HELPERS
-  // ==========================================================================
-
-  // Concat source files and/or directives.
-  function bom(files) {
-	if (files)
-	{
-		files.map(function(filepath)
-		{
-			var content = grunt.task.directive(filepath, grunt.file.read);
-			if (/^\uFEFF/.test(content))
-			{
-				content = content.replace(/^\uFEFF/, '');
-				grunt.file.write(filepath, content);
-				grunt.log.writeln('File "' + filepath + '" rewritten.');
-			}
-		});
-	}
-  }
-
+        // Fail task if errors were logged.
+        if (this.errorCount) { return false; }
+    });
 }
